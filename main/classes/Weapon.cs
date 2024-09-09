3d718e286da35558ddaf428f2f08624846fcd854
Kasmi-05 <l.zouaoui.rami13@gmail.com>
2024-09-09 09:56:05 +0200

    weapon class

---
+++ main/classes/Weapon.cs
@@ -0,0 +1,13 @@
+public class Weapon
+{
+    public int ID;
+    public int MaximumDamage;
+    public string Name;
+
+    public Weapon(int ID, int MaximumDamage, string Name)
+    {
+        this.ID = id
+        this.MaximumDamage = maxdamage
+        this.Name = name
+    }
+}
