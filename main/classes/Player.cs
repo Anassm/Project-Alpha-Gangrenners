137c17270635dffe3a02c97fe73cd661e9f001f5
RedRoan04 <roanvrossum@gmail.com>
2024-09-09 10:00:22 +0200

    Player updated

--- main/classes/Player.cs
+++ main/classes/Player.cs
@@ -5,4 +5,13 @@ public class Player
     public Weapon CurrentWeapon;
     public int MaximumHitPoints;
     public string Name;
+
+    public Player(int currentHitPoints, Location currentLocation, Weapon currentWeapon, int maximumHitPoints, string name)
+    {
+        this.CurrentHitPoints = currentHitPoints;
+        this.CurrentLocation = currentLocation;
+        this.CurrentWeapon = currentWeapon;
+        this.MaximumHitPoints = maximumHitPoints;
+        this.Name = name;
+    }
 }
