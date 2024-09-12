239e6f8d60a485ea1f2c427929a133a7afcc62ee
Kasmi-05 <l.zouaoui.rami13@gmail.com>
2024-09-12 20:05:14 +0200

    Inventory class dictonary

--- main/classes/Inventory.cs
+++ main/classes/Inventory.cs
@@ -1,4 +1,13 @@
 public class Inventory
 {
-    
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> inventory = new();
+
+        inventory.Add("WEAPON_ID_RUSTY_SWORD", 1);
+        inventory.Add("WEAPON_ID_CLUB", 0);
+        inventory.Add("HEALTH_ID_SMALL", 0);
+        inventory.Add("HEALTH_ID_MID", 0);
+        inventory.Add("HEALTH_ID_BIG", 0);
+    }
 }
