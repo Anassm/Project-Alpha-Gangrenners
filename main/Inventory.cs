100f835f4d154443cd9d63059a89b8cedd5ceb29
Kasmi-05 <l.zouaoui.rami13@gmail.com>
2024-09-16 16:20:49 +0200

    items class and file added

--- main/Inventory.cs
+++ main/Inventory.cs
@@ -8,9 +8,9 @@ public class Inventory
         {
             { "Rusty Sword", 1 },
             { "Club", 0 },
-            { "Small Potion", 0 },
+            { "Small Potion", 1 },
             { "Medium Potion", 0 },
-            { "Big Potion", 0 }
+            { "Big Potion", 0 },
         };
     }
 
@@ -31,15 +31,28 @@ public class Inventory
     {
         System.Console.WriteLine("Welcome to the inventory!");
         System.Console.WriteLine("You have: ");
-        
+        System.Console.WriteLine("Small Potion: " + inventory["Small Potion"]);
+        System.Console.WriteLine("Medium Potion: " + inventory["Medium Potion"]);
+        System.Console.WriteLine("Big Potion: " + inventory["Big Potion"]);
+
         if (!InCombat)
         {
             System.Console.WriteLine("Rusty Sword: " + inventory["Rusty Sword"]);
             System.Console.WriteLine("Club: " + inventory["Club"]);
         }
 
-        System.Console.WriteLine("Small Potion: " + inventory["Small Potion"]);
-        System.Console.WriteLine("Medium Potion: " + inventory["Medium Potion"]);
-        System.Console.WriteLine("Big Potion: " + inventory["Big Potion"]);
+        System.Console.WriteLine(" ");
+        System.Console.WriteLine("What would you like to do?");
+        System.Console.WriteLine("1. Use item (U)");
+        System.Console.WriteLine("2. Equip weapon (E)");
+        System.Console.WriteLine("3. Quit (Q)");
+        string choice = Console.ReadLine();
+        if (choice == "1" || choice == "u" || choice == "U")
+        {
+            System.Console.WriteLine("What item would you like to use?");
+            System.Console.WriteLine("1. Small Potion");
+            System.Console.WriteLine("2. Medium Potion");
+            System.Console.WriteLine("3. Big Potion");
+        }
     }
 }
---
+++ main/Item.cs
@@ -0,0 +1,15 @@
+public class Item
+{
+    public int ID;
+    public string Name;
+    public int Amount;
+    public int Value;
+
+    public Item(int id, string name, int amount, int value)
+    {
+        this.ID = id;
+        this.Name = name;
+        this.Amount = amount;
+        this.Value = value;
+    }
+}
