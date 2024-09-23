f5559fcc575022abb2592dc893242240761dcf4f
RedRoan04 <roanvrossum@gmail.com>
2024-09-23 13:10:41 +0200

    test commit with some changes

--- main/Text.cs
+++ main/Text.cs
@@ -2,7 +2,6 @@ public static class Text
 {
     public static void Alert(string message)
     {
-        // Add color to alert messages 
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine(message);
         Console.ResetColor();
@@ -10,7 +9,6 @@ public static class Text
 
     public static void Info(string message)
     {
-        // Add color to info messages 
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine(message);
         Console.ResetColor();
